1f22fc405e8a77b56db0e2c209a211f7b594ed03
Mark Heckler <mark.heckler@gmail.com>
2019-01-30 17:35:27 -0600

    Code cleanup

--- src/main/java/com/thehecklers/ssecoidc/SsecOidcApplication.java
+++ src/main/java/com/thehecklers/ssecoidc/SsecOidcApplication.java
@@ -3,17 +3,12 @@ package com.thehecklers.ssecoidc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.AuthenticatedPrincipalOAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.Map;
 
 @SpringBootApplication
 public class SsecOidcApplication {
